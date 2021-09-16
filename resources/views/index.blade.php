101f6dcab5196d2ecb0a85fcb40541bac71e7f79
aliansyah <muhammad.aliansyah@divusi.com>
2021-09-16 14:13:17 +0700

    Add template layouts stisla index, master, header & login form

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+@section('title', 'Laravel')
+@section('content')
+<div class="section-body">
+    Ini isi content <br>
+    {{-- Halo {{ Auth::user()->name }} <br>
+    Email Anda {{ Auth::user()->email }} --}}
+</div>
+@endsection
