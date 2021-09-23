9d416678781a972ab67d9d8156ea8628ad41619f
aliansyah <muhammad.aliansyah@divusi.com>
2021-09-23 15:37:08 +0700

    Create relation one to many inverse between Karyawan & Karyawan_details

--- app/Models/Karyawan.php
+++ app/Models/Karyawan.php
@@ -16,6 +16,11 @@ class Karyawan extends Model
         // return $this->belongsTo('\App\Jabatan', 'id_jab');
     }
 
+    public function karyawan_details()
+    {
+        return $this->hasOne('\App\Models\Karyawan_details', 'karyawan_id');
+    }
+
     public function karyawan_keluarga()
     {
         return $this->hasMany('\App\Models\Karyawan_keluarga', 'karyawan_id');
