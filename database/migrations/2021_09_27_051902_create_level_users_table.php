391ecd4997ee74e7ba0ce704f6f8ac821cc66527
aliansyah <muhammad.aliansyah@divusi.com>
2021-09-27 13:03:07 +0700

    Create migration level_users, menu, akses & add column level user to users table

--- database/migrations/2021_09_27_051902_create_level_users_table.php
+++ database/migrations/2021_09_27_051902_create_level_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnToUsersTable extends Migration
+class CreateLevelUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,10 @@ class AddColumnToUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('username', 20)->after('name');
+        Schema::create('level_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_level_user', 50);
+            $table->timestamps();
         });
     }
 
@@ -25,8 +27,6 @@ class AddColumnToUsersTable extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('username');
-        });
+        Schema::dropIfExists('level_users');
     }
 }
